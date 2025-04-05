1c6d6c1723e6184860658223461e4375a0ca9b20
suk2005 <154073877+suk2005@users.noreply.github.com>
2025-04-05 15:22:30 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -4,10 +4,10 @@ public class MyClass {
   public static void main(String[] args) {
     int x, y, sum;
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Type a number:");
+    System.out.println("number1:");
     x = myObj.nextInt(); // Read user input
 
-    System.out.println("Type another number:");
+    System.out.println("number2:");
     y = myObj.nextInt(); // Read user input
 
     sum = x + y;
